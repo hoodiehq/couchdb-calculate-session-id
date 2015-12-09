38e9697962d6a425e08050f6eea0f841cac492b1
Fedor Indutny <fedor@indutny.com>
2015-12-09 17:19:22 -0500

    fix: remove excessive buffer allocations

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ function generateSessionId (options) {
   var sessionData = options.username + ':' + timestamp
   var hmac = crypto.createHmac('sha1', options.salt + options.secret)
   hmac.update(sessionData)
-  var hash = hmac.digest()
 
   return Buffer.concat([
-    new Buffer(sessionData),
-    new Buffer(':'),
-    new Buffer(hash)
+    new Buffer(sessionData + ':'),
+    hmac.digest()
   ]).toString('base64').replace(/\+/g, '-')
 }
