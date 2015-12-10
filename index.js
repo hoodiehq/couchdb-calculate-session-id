119eaa34aed48e8c10619b8cafef64913b11e65b
Stephan Bönnemann <stephan@boennemann.me>
2015-12-10 15:22:16 +0100

    feat: validate arguments
    BREAKING CHANGE: Throws an Error when arguments are missing or of the wrong type, instead of ignoring or coercing.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ module.exports = generateSessionId
 var crypto = require('crypto')
 
 var base64url = require('base64url')
+var validate = require('aproba')
 
 function generateSessionId (username, salt, secret, timestamp) {
+  validate('SSSN', arguments)
+
   var timestamp16 = timestamp.toString(16).toUpperCase()
   var sessionData = username + ':' + timestamp16
   var hmac = crypto.createHmac('sha1', salt + secret).update(sessionData)
