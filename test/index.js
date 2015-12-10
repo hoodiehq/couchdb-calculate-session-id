7f11e4e255370a51aecf2306fb8c9b5a0b73b6e4
Stephan Bönnemann <stephan@boennemann.me>
2015-12-10 15:07:22 +0100

    test: use arguments rather than options object

--- test/index.js
+++ test/index.js
@@ -3,14 +3,13 @@ var test = require('tap').test
 var generateSessionId = require('../index')
 
 test('valid options', function (t) {
-  var options = {
-    username: 'jan',
-    timestamp: 1449689785,
-    salt: '4ed13457964f05535fbb54c0e9f77a83',
-    secret: 'd5513283df4f649c72757a91aa30bdde'
-  }
   var expected = 'amFuOjU2Njg4MkI5OkEK3-1SRseo6yNRHfk-mmk6zOxm'
-  var result = generateSessionId(options)
+  var result = generateSessionId(
+    'jan',
+    '4ed13457964f05535fbb54c0e9f77a83',
+    'd5513283df4f649c72757a91aa30bdde',
+    1449689785
+  )
 
   t.is(result, expected, 'returns expected result')
 
