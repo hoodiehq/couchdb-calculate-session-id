cc48be420cc9b8c3e1aac71a5cbf27730d053f54
Gregor Martynus <gregor@martynus.net>
2015-12-14 20:56:16 -0500

    feat: initial version

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = calculateSessionId
+
+var crypto = require('crypto')
+
+var base64url = require('base64url')
+var validate = require('aproba')
+
+function calculateSessionId (username, usersalt, serversecret, timestamp) {
+  validate('SSSN', arguments)
+
+  var timestamp16 = timestamp.toString(16).toUpperCase()
+  var sessionData = username + ':' + timestamp16
+  var hmac = crypto.createHmac('sha1', usersalt + serversecret).update(sessionData)
+
+  return base64url(Buffer.concat([
+    new Buffer(sessionData + ':'),
+    hmac.digest()
+  ]))
+}
