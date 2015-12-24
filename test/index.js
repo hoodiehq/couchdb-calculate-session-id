38c11b35d7ded31ad70267edccf90867375c0ac6
Gregor Martynus <gregor@martynus.net>
2015-12-24 22:50:51 +0100

    test: CouchDB 1.6 testcase

--- test/index.js
+++ test/index.js
@@ -6,9 +6,23 @@ test('valid arguments', function (t) {
   var expected = 'amFuOjU2Njg4MkI5OkEK3-1SRseo6yNRHfk-mmk6zOxm'
   var result = calculateSessionId(
     'jan',
-    '4ed13457964f05535fbb54c0e9f77a83',
     'd5513283df4f649c72757a91aa30bdde',
+    '4ed13457964f05535fbb54c0e9f77a83',
     1449689785
+  )
+
+  t.is(result, expected, 'returns expected result')
+
+  t.end()
+})
+
+test('debug CouchDB 1.6.1', function (t) {
+  var expected = 'dGVzdDQ3OWQ2MDI6NTY3QzE3NzE6BT0lIdzzNDAEqerXpzxOw1AZvsQ'
+  var result = calculateSessionId(
+    'test479d602',
+    '1ac16e1dd1e01201ec16d1941b119e1991e91d018a1af19b',
+    '78875068a1979fb910d5d8f37d316aa4',
+    1450973041
   )
 
   t.is(result, expected, 'returns expected result')
