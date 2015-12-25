0f6d4830e9faa8db95f3c6c2723768315989d656
Gregor Martynus <gregor@martynus.net>
2015-12-25 10:43:38 +0100

    refactor: test code readability

--- test/index.js
+++ test/index.js
@@ -2,30 +2,39 @@ var test = require('tap').test
 
 var calculateSessionId = require('../index')
 
-test('valid arguments', function (t) {
-  var expected = 'amFuOjU2Njg4MkI5OkEK3-1SRseo6yNRHfk-mmk6zOxm'
+test('valid arguments CouchDB 1.5', function (t) {
+  var expectedSessionId = 'amFuOjU2Njg4MkI5OkEK3-1SRseo6yNRHfk-mmk6zOxm'
+  var username = 'jan'
+  var usersalt = 'd5513283df4f649c72757a91aa30bdde'
+  var serversecret = '4ed13457964f05535fbb54c0e9f77a83'
+  var timestamp = 1449689785
   var result = calculateSessionId(
-    'jan',
-    'd5513283df4f649c72757a91aa30bdde',
-    '4ed13457964f05535fbb54c0e9f77a83',
-    1449689785
+    username,
+    usersalt,
+    serversecret,
+    timestamp
   )
 
-  t.is(result, expected, 'returns expected result')
+  t.is(result, expectedSessionId, 'returns expected session ID')
 
   t.end()
 })
 
-test('debug CouchDB 1.6.1', function (t) {
-  var expected = 'dGVzdDQ3OWQ2MDI6NTY3QzE3NzE6BT0lIdzzNDAEqerXpzxOw1AZvsQ'
+test('valid arguments CouchDB 1.6.1', function (t) {
+  var expectedSessionId = 'dGVzdDQ3OWQ2MDI6NTY3QzE3NzE6BT0lIdzzNDAEqerXpzxOw1AZvsQ'
+  var username = 'test479d602'
+  var usersalt = '1ac16e1dd1e01201ec16d1941b119e1991e91d018a1af19b'
+  var serversecret = '78875068a1979fb910d5d8f37d316aa4'
+  var timestamp = 1450973041
+
   var result = calculateSessionId(
-    'test479d602',
-    '1ac16e1dd1e01201ec16d1941b119e1991e91d018a1af19b',
-    '78875068a1979fb910d5d8f37d316aa4',
-    1450973041
+    username,
+    usersalt,
+    serversecret,
+    timestamp
   )
 
-  t.is(result, expected, 'returns expected result')
+  t.is(result, expectedSessionId, 'returns expected result')
 
   t.end()
 })
